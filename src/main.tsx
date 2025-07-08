e947ed12c2dff08a082ce2447599e2f623a33f8a
golgothr <gauthier.rey@lilo.org>
2025-07-08 18:07:06 +0200

    feat(auth): add global context provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { AppRouter } from './AppRouter';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './core/hooks/useAuth';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
